b59d682cba5df678dc6f64a5f1edb90a2fb1b84c
sylvainpillay <sylvain.pillay@chillisoft.co.za>
2020-02-04 19:49:19 +0200

    refactor code

--- StringCalculator/StringCalculator1.cs
+++ StringCalculator/StringCalculator1.cs
@@ -7,44 +7,49 @@ namespace StringCalculator
    public class StringCalculator1
     {
         // TODO the code for getting delimiters is spread out between this field, the Add method and the getDelimiterList
-        //       getting delimiters is definitely one responsibility.
-        private List<string> defaultDelimList = new List<string> { ",", "\n" };
+        //       getting delimiters is definitely one responsibility. - done
         public int Add(string args)
         {
-            // TODO I'm not convinced you need this line of code.
-            if (string.IsNullOrEmpty(args)) return 0;
 
+            var delimList = getDelimiterList(args);
+
+            args = GetStringToSplit(args);
+
+            return getSumOfAllNumbers(args, delimList);
+        }
+        private List<string> getDelimiterList(string args)
+        {
+            var delimList = new List<string> { ",", "\n" };
             if (args.StartsWith("//"))
             {
-                AddCustomDelimiter(ref args);
+                var delimString = args.Substring(2, args.IndexOf('\n') - 2);
+                delimList.AddRange(delimString.Split('[').Select(a => a.TrimEnd(']')).ToList());
+                delimList.Remove(string.Empty);
             }
-            return getSumOfAllNumbers(args);
+            return delimList;
         }
-
-        public void AddCustomDelimiter(ref string args)
+        private static string GetStringToSplit(string args)
         {
-            defaultDelimList.AddRange(getDelimiterList(args));
             args = args.Substring(args.IndexOf('\n') + 1);
+            return args;
         }
 
         // TODO this method is doing multiple things, it is parsing number, splitting numbers, filtering number and
-        //       doing a little co-ordination by calling the ValidateNegativeNumbers.
-        private int getSumOfAllNumbers(string args)
+        //       doing a little co-ordination by calling the ValidateNegativeNumbers. - done
+        private int getSumOfAllNumbers(string args, List<string> delimList)
         {
-            var numberList = args.Split(defaultDelimList.ToArray(), StringSplitOptions.None)
+            var numberList = GetNumberList(args, delimList);
+            return numberList.Where(a => a <= 1000).Sum();
+        }
+
+        private List<int> GetNumberList(string args, List<string> delimList)
+        {
+            var numberList = args.Split(delimList.ToArray(), StringSplitOptions.None)
                 .Where(a => int.TryParse(a, out _)).Select(int.Parse).ToList();
 
             ValidateNegativeNumbers(numberList);
 
-            return numberList.Where(a => a < 1000).Sum();
-        }
-
-        public List<string> getDelimiterList(string args)
-        {
-            var delimString = args.Substring(2, args.IndexOf('\n') - 2);
-            var delimList = delimString.Split('[').Select(a => a.TrimEnd(']')).ToList();
-            delimList.Remove(string.Empty);
-            return delimList;
+            return numberList;
         }
 
         private void ValidateNegativeNumbers(List<int> numbers)
--- TestStringCalculator/TestStringCalculator1.cs
+++ TestStringCalculator/TestStringCalculator1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection.Metadata.Ecma335;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using StringCalculator;
 using Assert = NUnit.Framework.Assert;
@@ -25,25 +26,25 @@ namespace TestStringCalculator
     //
     //       You are more than welcome to use the [TestCase] attribute.
     //
-    // TODO the files in the Bin & Obj folder have unfortunately become tracked in this repository
+    // TODO the files in the Bin & Obj folder have unfortunately become tracked in this repository - done
     //       please research how to untrack them and then action that research.
-    // TODO please update these tests to use NUnit, MSTest is the pain
-    // TODO given your new found knowledge try and improve the test names
-    // TODO Tests should have //Arrange //Act //Assert comments
+    // TODO please update these tests to use NUnit, MSTest is the pain - done
+    // TODO given your new found knowledge try and improve the test names - done
+    // TODO Tests should have //Arrange //Act //Assert comments - done
     [TestFixture]
-    public class TestStringCalculator1 // TODO why does this class have the number 6 at the end
+    public class TestStringCalculator1 
     {
-        // TODO, tests should not share state. Doing so couples them and allows them to interact, which is bad.
-        //        Please create an instance per test in their arrange sections using a factory function.
+        // TODO, tests should not share state. Doing so couples them and allows them to interact, which is bad. 
+        //        Please create an instance per test in their arrange sections using a factory function. - done
 
         public StringCalculator1 StringCalculatorSutBuilder()
         {
-            var useCase = new  StringCalculator1();
+            var useCase = new StringCalculator1();
             return useCase;
         }
 
-        [Test]
-        public void Add_PassedEmptyString_Returns0()
+        [Test] 
+        public void Add_GivenInput_Is_Empty_ShouldReturnZero()
         {
             //Arrange
             var input = "";
@@ -56,12 +57,12 @@ namespace TestStringCalculator
         }
 
         // TODO triangulate, refer to TODO comment on class
-        [Test]
-        public void Add_Passed1Number_Returns1()
+        [TestCase("1", 1)]
+        [TestCase("25", 25)]
+        [TestCase("100",100)]
+        public void Add_GivenOnlySingleNumber_ShouldReturnNumber(string input, int expectedResult)
         {
             //Arrange
-            var input = "1";
-            var expectedResult = 1; 
             var useCase = StringCalculatorSutBuilder();
             //Act
             var result = useCase.Add(input);
@@ -71,12 +72,12 @@ namespace TestStringCalculator
 
         // TODO triangulate, refer to TODO comment on class
         // TODO, this test doesn't have an assert
-        [Test]
-        public void Add_PassedTwoNumber_Returns4()
+        [TestCase("1,1", 2)]
+        [TestCase("25,35", 60)]
+        [TestCase("100,200", 300)]
+        public void Add_GivenTwoNumbers_ShouldReturnSumOfNumbers(string input, int expectedResult)
         {
             //Arrange
-            var input = "2,2";
-            var expectedResult = 4;
             var useCase = StringCalculatorSutBuilder();
             //Act
             var result = useCase.Add(input);
@@ -85,12 +86,12 @@ namespace TestStringCalculator
         }
 
         // TODO triangulate, refer to TODO comment on class
-        [Test]
-        public void Add_PassedCustomDelimiter_Returns8()
+        [TestCase("//;\n2;2;2;2", 8)]
+        [TestCase("//$\n23$31,62$2", 118)]
+        [TestCase("//z\n200z278\n2", 480)]
+        public void Add_GivenCustomDelimiterAvailable_ShouldSplitWithDelimiter_AndSumAllNumbers(string input, int expectedResult)
         {
             //Arrange
-            var input = "//;\n2;2;2;2";
-            var expectedResult = 8;
             var useCase = StringCalculatorSutBuilder();
             //Act
             var result = useCase.Add(input);
@@ -99,24 +100,25 @@ namespace TestStringCalculator
         }
 
         // TODO triangulate, refer to TODO comment on class
-        [Test]
-        public void Add_NegativesError()
+        [TestCase("//;\n2;-92;2;-2")]
+        [TestCase("//$\n-37$87")]
+        [TestCase("-74,98")]
+        public void Add_GivenNegativeNumberFound_ShouldThrowError(string input)
         {
             //Arrange
-            var input = "//;\n2;2;2;-2";
             var useCase = StringCalculatorSutBuilder();
             //Act + Assert
             var ex = Assert.Throws<Exception>(() => useCase.Add(input));
-            Assert.That(ex.Message, Is.EqualTo("negatives not allowed -2"));
+            Assert.That(ex.Message.Contains("negatives not allowed"));
         }
 
         // TODO triangulate, refer to TODO comment on class
-        [Test]
-        public void Add_PassedNumbersGreaterThan1000_Returns8()
+        [TestCase("//;\n2;2;2;2,1002", 8)]
+        [TestCase("//;\n999;0;7;36,1001", 1042)]
+        [TestCase("//;\n1000;87;1002", 1087)]
+        public void Add_GivenInputContainsNumbersGreaterThan_1000_ShouldNotSumNumber(string input, int expectedResult)
         {
-            //Arrange
-            var input = "//;\n2;2;2;2,1002";
-            var expectedResult = 8; 
+            //Arrange 
             var useCase = StringCalculatorSutBuilder();
             //Act
             var result = useCase.Add(input);
@@ -125,12 +127,13 @@ namespace TestStringCalculator
         }
 
         // TODO triangulate, refer to TODO comment on class
-        [Test]
-        public void Add_PassedMoreThanOneDelimiter_Returns6()
+        [TestCase("//[*][%]\n1*2%3", 6)]
+        [TestCase("//[a][z]\n98z213a8a32", 351)]
+        [TestCase("//[*][%][@]\n1*2%3@8", 14)]
+        public void Add_GivenMoreThanOneCustomerDelimiter_ShouldSplitStringUsingAllDelimiters_AndSum(string input, int expectedResult)
         {
             //Arrange
-            var input = "//[*][%]\n1*2%3";
-            var expectedResult = 6; 
+
             var useCase = StringCalculatorSutBuilder();
             //Act
             var result = useCase.Add(input);
@@ -139,19 +142,29 @@ namespace TestStringCalculator
         }
 
         // TODO triangulate, refer to TODO comment on class
-        [Test]
-        public void Add_PassedDelimiterOfAnyLen_Returns6()
+        [TestCase("//[***]\n1***2***3", 6)]
+        [TestCase("//[$$]\n10$$20$$30", 60)]
+        [TestCase("//[asd]\n100asd200", 300)]
+        public void Add_GivenCustomDelimiterOfAnyLength_ShouldSplitStringAndSum(string input, int expectedResult)
         {
             //Arrange
-            var input = "//[***]\n1***2***3";
-            var expectedResult = 6; 
             var useCase = StringCalculatorSutBuilder();
             //Act
             var result = useCase.Add(input);
             //Assert
             Assert.AreEqual(expectedResult, result);
         }
-
-        // TODO missing test for multiple delimiters of different lengths 
+        [TestCase("//[***][%%]\n1***2%%3", 6)]
+        [TestCase("//[$$][!!!]\n10!!!20$$30", 60)]
+        [TestCase("//[asd][pos]\n100asd200pos1pos9", 310)]
+        public void Add_GivenMultipleCustomDelimitersOfAnyLength_ShouldSplitStringAndSum(string input, int expectedResult)
+        {
+            //Arrange
+            var useCase = StringCalculatorSutBuilder();
+            //Act
+            var result = useCase.Add(input);
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
