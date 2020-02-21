6ce0d1a7d5790fb47c8b0c4ad1202fc3d07efc45
Sylvain Pillay <sylvain.pillay@chillisoft.co.za>
2020-02-21 17:01:00 +0200

    Add Calculator 2

---
+++ StringCalculator/StringCalculator2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using Microsoft.VisualBasic;
+
+namespace StringCalculator
+{
+    public class StringCalculator2
+    {
+        public int Add(string input)
+        {
+            if(input == "")return 0;
+            var delimiterList = GetDelimiterList(input);
+
+            var numberList = GetNumberList(input, delimiterList);
+
+
+            var numbersLessThan1001 = GetNumbersLessThanOrEqualTo1000(numberList);
+            ValidateNegativeNumbers(numbersLessThan1001);
+            
+
+            return GetSumOfAllNumbers(numbersLessThan1001);
+        }
+
+        private List<int> GetNumbersLessThanOrEqualTo1000(List<int> numberList)
+        {
+            return numberList.Where(a => a <= 1000).ToList();
+        }
+
+        private static void ValidateNegativeNumbers(List<int> numberList)
+        {
+            if (!numberList.Any(a => a < 0)) return;
+            throw new Exception($"negatives not allowed {GetNegativeNumbers(numberList)}");
+        }
+
+        private static string GetNegativeNumbers(List<int> numberList)
+        {
+           return string.Join(",", numberList.Where(a => a < 0).Select(a => a.ToString()).ToArray());
+        }
+
+        private static int GetSumOfAllNumbers(List<int> numberList)
+        {
+            return numberList.Sum();
+        }
+
+        private static List<int> GetNumberList(string input, List<string> delimiterList)
+        {
+            return input.Split(delimiterList.ToArray(), StringSplitOptions.None)
+                .Where(a => int.TryParse(a, out _)).Select(int.Parse).ToList();
+        }
+
+        private static List<string> GetDelimiterList(string args)
+        {
+            var delimiterList = new List<string>{",","\n"};
+            if (args.StartsWith("//"))
+            {
+                var customDelimiterList = GetCustomDelimiterList(args);
+                delimiterList.AddRange(customDelimiterList);
+            }
+
+            return delimiterList;
+        }
+
+        private static List<string> GetCustomDelimiterList(string args)
+        {
+            var customDelimiterString = args.Substring(2, args.IndexOf('\n') - 2);
+            var customDelimiterList = customDelimiterString.Split('[').Select(a => a.TrimEnd(']')).ToList();
+            return customDelimiterList;
+        }
+    }
+}
---
+++ TestStringCalculator/TestStringCalculator2.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using NUnit.Framework;
+using StringCalculator;
+
+namespace TestStringCalculator
+{
+    public class TestStringCalculator2
+    {
+        [Test]
+        public void Add_GivenEmptyString_ShouldReturnZero()
+        {
+            //Arrange
+            var useCase = SutBuilder();
+            var input = "";
+            var expectedResult = 0;
+            //Act
+            var result = useCase.Add(input);
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase("1",1)]
+        [TestCase("10",10)]
+        [TestCase("100",100)]
+        public void Given_SingleNumber_ShouldReturnNumber(string input, int expected)
+        {
+            //Arrange
+            var useCase = SutBuilder();
+            //Act
+            var result = useCase.Add(input);
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        [TestCase("1,1",2)]
+        [TestCase("10,50",60)]
+        [TestCase("100,250",350)]
+        public void Add_GivenTwoNumbers_ShouldReturnSum(string input, int expected)
+        {
+            //Arrange
+            var useCase = SutBuilder();
+            //Act
+            var result = useCase.Add(input);
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase("1,1\n7",9)]
+        [TestCase("10\n12,8",30)]
+        [TestCase("100,100\n93",293)]
+        public void Add_GivenNewLinesBetweenNumbers_ShouldAdd(string input, int expected)
+        {
+            //Arrange
+            var useCase = SutBuilder();
+            //Act
+            var result = useCase.Add(input);
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        [TestCase("//a\n1,1\n7a2",11)]
+        [TestCase("//#\n10\n12#8", 30)]
+        [TestCase("//=\n100=100=93=7", 300)]
+        public void Add_GivenCustomDelimiterSupplied_ShouldSplitString_AndAddNumbers(string input, int expected)
+        {
+            //Arrange
+            var useCase = SutBuilder();
+            //Act
+            var result = useCase.Add(input);
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        [TestCase("//a\n1,-11\n7a2", "negatives not allowed -11")]
+        [TestCase("//#\n-10\n-12#-8", "negatives not allowed -10,-12,-8")]
+        [TestCase("//=\n-100=-100=-93=7", "negatives not allowed -100,-100,-93")]
+        public void Add_GivenNegativeNumbers_ShouldThrowError(string input, string expectedMessage)
+        {
+            //Arrange
+            var useCase = SutBuilder();
+            //Act + Assert
+            var ex = Assert.Throws<Exception>(() => useCase.Add(input));
+            Assert.That(ex.Message.Equals(expectedMessage));
+        }
+        [TestCase("//a\n1,1\n7a2,1001", 11)]
+        [TestCase("//#\n10\n12#8,999", 1029)]
+        [TestCase("//=\n1000,100=100=93=7", 1300)]
+        public void Add_ShouldNotAdd_NumbersGreaterThan1000(string input, int expected)
+        {
+            //Arrange
+            var useCase = SutBuilder();
+            //Act
+            var result = useCase.Add(input);
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        [TestCase("//[aa]\n1,1\n7aa2,1001", 11)]
+        [TestCase("//[#33]\n10\n12#338,999", 1029)]
+        [TestCase("//[=+=]\n1000,100=+=100=+=93=+=7", 1300)]
+        public void Add_GivenCustomDelimiterOfAnyLength_ShouldSum(string input, int expected)
+        {
+            //Arrange
+            var useCase = SutBuilder();
+            //Act
+            var result = useCase.Add(input);
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        [TestCase("//[aa][aq]\n1,1\n7aa2aq1001", 11)]
+        [TestCase("//[#33][zw]\n10\n12#338zw999", 1029)]
+        [TestCase("//[=+=][ex]\n1000ex100=+=100=+=93=+=7", 1300)]
+        public void Add_GivenMultipleCustomDelimiterOfAnyLength_ShouldSum(string input, int expected)
+        {
+            //Arrange
+            var useCase = SutBuilder();
+            //Act
+            var result = useCase.Add(input);
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        private static StringCalculator2 SutBuilder()
+        {
+            return new StringCalculator2();
+        }
+    }
+}
